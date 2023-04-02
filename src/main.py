5977947303310295d502ecefdd91ad2635677a26
Richard Y. Hayashi <rhayashi@gruuw.com>
2023-04-02 21:05:17 +0900

    Added Get one Post.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -15,6 +15,10 @@ class Post(BaseModel):
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2}]
 
+def find_post(id):
+    for p in my_posts:
+        if p['id']==id:
+            return p
 
 @app.get( "/" )
 async def root():
@@ -26,9 +30,17 @@ def get_posts():
     return { "data": my_posts }
 
 
-@app.post( "/posts" )
+@app.post( "/posts", status_code=status.HTTP_201_CREATED )
 def create_posts( post: Post ):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 10000000)
     my_posts.append( post_dict )
     return { "data": post_dict }
+
+
+@app.get( "/posts/{id}" )
+def get_post( id: int ):
+    post=find_post( id )
+    if not post:
+        raise HTTPException( status_code=status.HTTP_404_NOT_FOUND, detail= f"post with id: {id} was not found!" )
+    return { "post_detail": post }
